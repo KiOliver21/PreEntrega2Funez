4185c2bb4c461678cdd95ebb26ed8149076bd75a
Facundo Oliver <facundofunez21@gmail.com>
2023-08-22 19:31:52 -0300

    temporary fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,20 @@
+/*HAMBURGER*/
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+));
+
 
 /*CART PAGE*/
 
@@ -167,7 +184,6 @@ function displayCart()
         `;
     }
 }
-
 
 onLoadCartNumbers();
 displayCart();
